320111101db5d599773caeed3a71543dc36e94ec
Shubham27072002 <27shubhamprajapati@gmail.com>
2026-05-11 07:01:20 +0000

    LAB2 updates

--- catkin_ws/src/lab1/scripts/my_turtlebot.py
+++ catkin_ws/src/lab1/scripts/my_turtlebot.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
-
+import rospy
+import sys
+import random
+from geometry_msgs.msg import Twist, PoseStamped, Quaternion
+from nav_msgs.msg import Odometry
+import math
+import time
 
 class myTurtle():
-    
-    
     def __init__(self) -> None:
-        """_summary_
-        
+        """_summary_     
         create all the nessary pubs/subs here and all the nessary other things
-        
         """
-        pass 
-    
-        
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_cb)
+        self.goal_sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.nav_to_pose)
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
+        self.linear_vel = 0.0
+        self.angular_vel = 0.0
+        self.rate = rospy.Rate(10)
 
     def nav_to_pose(self, goal):
         # type: (PoseStamped) -> None
@@ -23,44 +31,84 @@ class myTurtle():
         :param goal: PoseStamped
         :return:
         """
-        pass
+        goal_x = goal.pose.position.x
+        goal_y = goal.pose.position.y
+        dx = goal_x - self.x
+        dy = goal_y - self.y
+        target_angle = math.atan2(dy, dx)
+        angle_to_turn = target_angle - self.theta
+        # normalize angle between -pi and pi
+        if angle_to_turn > math.pi:
+            angle_to_turn -= 2 * math.pi
+        elif angle_to_turn < -math.pi:
+            angle_to_turn += 2 * math.pi
+        distance = math.sqrt(dx**2 + dy**2)
+        self.rotate(angle_to_turn)
+        self.drive_straight(distance, 0.15)
+        final_theta = self.convert_to_euler(goal.pose.orientation)
+        final_turn = final_theta - self.theta
+        # normalize final angle
+        if final_turn > math.pi:
+            final_turn -= 2 * math.pi
+        elif final_turn < -math.pi:
+            final_turn += 2 * math.pi
+        self.rotate(final_turn)
 
     def odom_cb(self,msg:Odometry) ->None:
         """_summary_
-
         Get the odom and update the internal location of the robot
         Args:
             msg (Odometry): _description_
         """
-        pass
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+        self.theta = self.convert_to_euler(msg.pose.pose.orientation)
+        self.linear_vel = msg.twist.twist.linear.x
+        self.angular_vel = msg.twist.twist.angular.z
     
     def stop(self)->None:
         """_summary_
-        
         Stop moving
         """
-        pass
-        
-        
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.angular.z = 0.0
+        self.cmd_vel_pub.publish(msg)
+   
     def drive_straight(self, dist: float, vel: float)->None:
         """_summary_
-
         Args:
             dist (_type_): _description_
         """
-        pass
-        
-    
-    def spin_wheels(self, u1, u2, time):
+        start_x = self.x
+        start_y = self.y
+        msg = Twist()
+        msg.linear.x = vel
+        msg.angular.z = 0.0
+        travelled = 0.0
+        while travelled < dist and not rospy.is_shutdown():
+            self.cmd_vel_pub.publish(msg)
+            travelled = math.sqrt((self.x - start_x) ** 2 + (self.y - start_y) ** 2)
+            self.rate.sleep()
+        self.stop()
+
+    def spin_wheels(self, u1, u2, time_duration):
         """
         Spin the two wheels
-
         :param u1: wheel 1 speed
         :param u2: wheel 2 speed
         :param time: time to drive
         :return: None
         """
-        pass
+        msg = Twist()
+        # Convert wheel speeds to linear and angular velocity
+        msg.linear.x = (u1 + u2) / 2.0
+        msg.angular.z = (u2 - u1)
+        start_time = rospy.Time.now().to_sec()
+        while (rospy.Time.now().to_sec() - start_time) < time_duration:
+            self.cmd_vel_pub.publish(msg)
+            self.rate.sleep()
+        self.stop()
 
     def rotate(self, angle):
         """
@@ -68,26 +116,115 @@ class myTurtle():
         :param angle: angle to rotate
         :return: None
         """
-        pass
+        start_theta = self.theta
+        msg = Twist()
+        msg.linear.x = 0.0
+        angular_speed = 0.3
+        if angle < 0:
+            angular_speed = -0.3
+        msg.angular.z = angular_speed
+        rotated = 0.0
+        while abs(rotated) < abs(angle) and not rospy.is_shutdown():
+            self.cmd_vel_pub.publish(msg)
+            rotated = self.theta - start_theta
+            # handle angle wraparound
+            if rotated > math.pi:
+                rotated -= 2 * math.pi
+            elif rotated < -math.pi:
+                rotated += 2 * math.pi
+            self.rate.sleep()
+        self.stop()
     
-    def convert_to_euler(self, quat):
+    def convert_to_euler(self, quat: Quaternion) -> float:
         # type: (Quaternion) -> float
         """
         This might be helpful to have
         :param quat: quaternion 
         :return: euler angles
         """
-        
+        x = quat.x
+        y = quat.y
+        z = quat.z
+        w = quat.w
+        # Yaw calculation
+        siny_cosp = 2 * (w * z + x * y)
+        cosy_cosp = 1 - 2 * (y * y + z * z)
+        yaw = math.atan2(siny_cosp, cosy_cosp)
+        return yaw
+
+    def drive_circle(self, radius) -> None:
+        """
+        Drive the robot in a circle of given radius
+        """
+        msg = Twist()
+        linear_speed = 0.15
+        angular_speed = linear_speed / radius
+        msg.linear.x = linear_speed
+        msg.angular.z = angular_speed
+        duration = (2 * math.pi) / angular_speed
+        start_time = rospy.Time.now().to_sec()
+        while (rospy.Time.now().to_sec() - start_time) < duration and not rospy.is_shutdown():
+            self.cmd_vel_pub.publish(msg)
+            self.rate.sleep()
+        self.stop()
+
+    def spinning_wheels(self, duration: float) -> None:
+        """
+        Spins the TurtleBot3 in place for a given duration using spin_wheels().
+        Args:
+            duration (float): Time in seconds for which the robot should spin
+        """
+        wheel_speed = 0.2
+        self.spin_wheels(wheel_speed, -wheel_speed, duration)
 
 def main():
     """_summary_
     create all the node start up here
     """
-    
-    
-
-
-
+    rospy.init_node('my_turtlebot_node')
+    robot = myTurtle()
+    rospy.sleep(2)
+    rospy.loginfo("TurtleBot node started.")
+    if len(sys.argv) < 2:
+        rospy.loginfo("No task provided.")
+        rospy.loginfo("Usage: rosrun lab1 my_turtlebot.py [q5|q6|q7|q8]")
+        return
+    task = sys.argv[1]
+    if task == "q5":
+        rospy.loginfo("Running Question 5: drive in a circle of radius 0.5 m")
+        robot.drive_circle(0.5)
+    elif task == "q6":
+        rospy.loginfo("Running Question 6: square path with side 0.5 m")
+        for _ in range(4):
+            robot.drive_straight(0.5, 0.15)
+            rospy.sleep(1)
+            robot.rotate(math.pi / 2)
+            rospy.sleep(1)
+        robot.stop()
+    elif task == "q7":
+        rospy.loginfo("Running Question 7: use RViz 2D Nav Goal")
+        rospy.spin()
+    elif task == "q8":
+        rospy.loginfo("Running Question 8: random dance")
+        for _ in range(10):
+            move = random.choice(["straight", "rotate_left", "rotate_right", "spin"])
+            if move == "straight":
+                rospy.loginfo("Dance move: drive straight")
+                robot.drive_straight(0.3, 0.15)
+            elif move == "rotate_left":
+                rospy.loginfo("Dance move: rotate left")
+                robot.rotate(math.pi / 2)
+            elif move == "rotate_right":
+                rospy.loginfo("Dance move: rotate right")
+                robot.rotate(-math.pi / 2)
+            elif move == "spin":
+                rospy.loginfo("Dance move: spin in place")
+                robot.spinning_wheels(2)
+            rospy.sleep(1)
+        robot.stop()
+    else:
+        rospy.loginfo("Invalid task argument.")
+        rospy.loginfo("Usage: rosrun lab1 my_turtlebot.py [q5|q6|q7|q8]")
 
 if __name__ == '__main__':
     main()
---
+++ catkin_ws/src/lab2/scripts/astar_planner.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python3
+
+import rospy
+import queue as Queue
+import math
+
+from nav_msgs.msg import OccupancyGrid, GridCells, Path
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, Point
+
+
+class AStarPlanner:
+    def __init__(self):
+        rospy.init_node("astar_planner")
+
+        self.map_data = None
+        self.start = None
+        self.goal = None
+        self.inflated_obstacles = set()
+
+        self.expanded_pub = rospy.Publisher("/expanded_cells", GridCells, queue_size=1, latch=True)
+        self.frontier_pub = rospy.Publisher("/frontier_cells", GridCells, queue_size=1, latch=True)
+        self.unexplored_pub = rospy.Publisher("/unexplored_cells", GridCells, queue_size=1, latch=True)
+        self.inflated_pub = rospy.Publisher("/inflated_obstacles", GridCells, queue_size=1, latch=True)
+        self.path_cells_pub = rospy.Publisher("/path_cells", GridCells, queue_size=1, latch=True)
+        self.path_pub = rospy.Publisher("/planned_path", Path, queue_size=1, latch=True)
+
+        rospy.Subscriber("/map", OccupancyGrid, self.map_callback)
+        rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, self.start_callback)
+        rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.goal_callback)
+
+        rospy.loginfo("A* planner node started.")
+        rospy.spin()
+
+    def map_callback(self, msg):
+        self.map_data = msg
+        self.inflated_obstacles = self.inflate_obstacles(padding_cells=2)
+        self.publish_gridcells(self.inflated_obstacles, self.inflated_pub)
+        rospy.loginfo_once("Map received and inflated obstacles published.")
+
+    def start_callback(self, msg):
+        if self.map_data is None:
+            rospy.logwarn("Map not received yet.")
+            return
+
+        self.start = self.world_to_grid(
+            msg.pose.pose.position.x,
+            msg.pose.pose.position.y
+        )
+
+        rospy.loginfo("Start set: %s", str(self.start))
+        self.try_plan()
+
+    def goal_callback(self, msg):
+        if self.map_data is None:
+            rospy.logwarn("Map not received yet.")
+            return
+
+        self.goal = self.world_to_grid(
+            msg.pose.position.x,
+            msg.pose.position.y
+        )
+
+        rospy.loginfo("Goal set: %s", str(self.goal))
+        self.try_plan()
+
+    def try_plan(self):
+        if self.map_data is None or self.start is None or self.goal is None:
+            return
+
+        path, expanded, frontier = self.astar(self.start, self.goal)
+
+        # self.publish_gridcells(expanded, self.expanded_pub)
+        # self.publish_gridcells(frontier, self.frontier_pub)
+        # self.publish_path(path)
+
+        rospy.loginfo("A* complete. Path length: %d", len(path))
+
+    def astar(self, start, goal):
+        open_set = Queue.PriorityQueue()
+        open_set.put((0, start))
+
+        came_from = {start: None}
+        cost_so_far = {start: 0}
+
+        expanded = set()
+        frontier = set()
+        unexplored = set()
+
+        rate = rospy.Rate(20)   # controls RViz update speed
+
+        while not open_set.empty() and not rospy.is_shutdown():
+            current_priority, current = open_set.get()
+
+            if current in expanded:
+                continue
+
+            expanded.add(current)
+
+            if current in frontier:
+                frontier.remove(current)
+
+            if current == goal:
+                break
+
+            x, y = current
+            neighbors = [
+                (x + 1, y),
+                (x - 1, y),
+                (x, y + 1),
+                (x, y - 1),
+                (x + 1, y + 1),
+                (x - 1, y + 1),
+                (x + 1, y - 1),
+                (x - 1, y - 1)
+            ]
+
+            for next_node in neighbors:
+                if not self.is_cell_free(next_node):
+                    continue
+
+                new_cost = cost_so_far[current] + 1
+
+                if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
+                    cost_so_far[next_node] = new_cost
+                    priority = new_cost + self.heuristic(next_node, goal)
+
+                    open_set.put((priority, next_node))
+                    came_from[next_node] = current
+
+                    if next_node not in expanded:
+                        frontier.add(next_node)
+
+            # unexplored = free cells not expanded and not frontier
+            unexplored.clear()
+            width = self.map_data.info.width
+            height = self.map_data.info.height
+
+            for y in range(height):
+                for x in range(width):
+                    cell = (x, y)
+                    if self.is_cell_free(cell) and cell not in expanded and cell not in frontier:
+                        unexplored.add(cell)
+
+            # publish real-time search state
+            self.publish_gridcells(expanded, self.expanded_pub)
+            self.publish_gridcells(frontier, self.frontier_pub)
+            self.publish_gridcells(unexplored, self.unexplored_pub)
+
+            rate.sleep()
+
+        path = self.reconstruct_path(came_from, start, goal)
+
+        # publish final result
+        self.publish_gridcells(expanded, self.expanded_pub)
+        self.publish_gridcells(frontier, self.frontier_pub)
+        self.publish_gridcells(unexplored, self.unexplored_pub)
+        self.publish_path(path)
+
+        return path, expanded, frontier
+
+    def heuristic(self, node, goal):
+        return math.sqrt((node[0] - goal[0])**2 + (node[1] - goal[1])**2)
+
+    def is_cell_free(self, cell):
+        x, y = cell
+
+        width = self.map_data.info.width
+        height = self.map_data.info.height
+
+        if x < 0 or y < 0 or x >= width or y >= height:
+            return False
+
+        index = y * width + x
+        value = self.map_data.data[index]
+
+        # -1 = unknown, 0 = free, 100 = occupied
+        if cell in self.inflated_obstacles:
+            return False
+
+        return value == 0
+
+    def reconstruct_path(self, came_from, start, goal):
+        if goal not in came_from:
+            rospy.logwarn("No path found.")
+            return []
+
+        current = goal
+        path = []
+
+        while current is not None:
+            path.append(current)
+            current = came_from[current]
+
+        path.reverse()
+        return path
+
+    def world_to_grid(self, x, y):
+        origin_x = self.map_data.info.origin.position.x
+        origin_y = self.map_data.info.origin.position.y
+        resolution = self.map_data.info.resolution
+
+        grid_x = int((x - origin_x) / resolution)
+        grid_y = int((y - origin_y) / resolution)
+
+        return grid_x, grid_y
+
+    def grid_to_world(self, cell):
+        x, y = cell
+
+        origin_x = self.map_data.info.origin.position.x
+        origin_y = self.map_data.info.origin.position.y
+        resolution = self.map_data.info.resolution
+
+        world_x = origin_x + (x + 0.5) * resolution
+        world_y = origin_y + (y + 0.5) * resolution
+
+        return world_x, world_y
+    
+    def inflate_obstacles(self, padding_cells=2):
+        inflated = set()
+        original_obstacles = set()
+
+        width = self.map_data.info.width
+        height = self.map_data.info.height
+
+        for y in range(height):
+            for x in range(width):
+                index = y * width + x
+                value = self.map_data.data[index]
+
+                if value > 50:
+                    original_obstacles.add((x, y))
+
+        for obs in original_obstacles:
+            ox, oy = obs
+
+            for dy in range(-padding_cells, padding_cells + 1):
+                for dx in range(-padding_cells, padding_cells + 1):
+                    nx = ox + dx
+                    ny = oy + dy
+
+                    if 0 <= nx < width and 0 <= ny < height:
+                        inflated.add((nx, ny))
+
+        # remove actual obstacle cells from inflated display
+        inflated_padding_only = inflated - original_obstacles
+
+        return inflated_padding_only
+
+    def publish_gridcells(self, cells, publisher):
+        msg = GridCells()
+        msg.header.frame_id = "map"
+        msg.header.stamp = rospy.Time.now()
+
+        msg.cell_width = self.map_data.info.resolution
+        msg.cell_height = self.map_data.info.resolution
+
+        for cell in cells:
+            x, y = self.grid_to_world(cell)
+            point = Point()
+            point.x = x
+            point.y = y
+            point.z = 0.0
+            msg.cells.append(point)
+
+        publisher.publish(msg)
+
+    def publish_path(self, path):
+        msg = Path()
+        msg.header.frame_id = "map"
+        msg.header.stamp = rospy.Time.now()
+
+        for cell in path:
+            x, y = self.grid_to_world(cell)
+
+            pose = PoseStamped()
+            pose.header.frame_id = "map"
+            pose.header.stamp = rospy.Time.now()
+            pose.pose.position.x = x
+            pose.pose.position.y = y
+            pose.pose.position.z = 0.0
+            pose.pose.orientation.w = 1.0
+
+            msg.poses.append(pose)
+        self.publish_gridcells(path, self.path_cells_pub)
+        self.path_pub.publish(msg)
+
+
+if __name__ == "__main__":
+    try:
+        AStarPlanner()
+    except rospy.ROSInterruptException:
+        pass
